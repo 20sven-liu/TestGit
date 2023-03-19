2ee586fcebc24f0010104f98a7b9154b7d50aca2
lensk <2939520434@qq.com>
2023-03-19 22:28:05 +0800

    第二次提交 Test文件

--- Test.java
+++ Test.java
@@ -2,5 +2,7 @@ public class Test{
     public static void main(String args[]){
         // first write -- first commit
         System.out.println("this is lensk");
+        // seconde write 
+        System.out.println("second write");
     }
 }
